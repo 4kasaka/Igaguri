1790d7a00d10f2e5d06c67fc0a9e0135b71104be
Koutexya <fumitan0129@gmail.com>
2024-10-01 12:23:43 +0900

    10/1 追加更新

--- Assets/Script/IgaguriGenerator.cs
+++ Assets/Script/IgaguriGenerator.cs
@@ -14,7 +14,9 @@ public class IgaguriGenerator : MonoBehaviour
         {
 
             GameObject igaguri = Instantiate(igaguriPrefab);
-            igaguri.GetComponent<IgaguriContoller>().Shoot(new Vector3(0, 200, 2000));
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 wolrdDir = ray.direction;
+            igaguri.GetComponent<IgaguriContoller>().Shoot((wolrdDir.normalized*2000));
         
         }
     }
