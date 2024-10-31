097b2a516135015839c4a86e4e4e4dc6177ec506
406-08\student <fca>
2024-10-31 17:01:39 +0900

    いがぐり完成

--- Assets/Script/IgaguriGenerator.cs
+++ Assets/Script/IgaguriGenerator.cs
@@ -2,22 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IgaguriGenerator : MonoBehaviour
+public class igaguriGenerator : MonoBehaviour
 {
-    public GameObject igaguriPrefab;
-    // Start is called before the first frame update
-   
+
+    public GameObject igaguriPrafab;
     // Update is called once per frame
     void Update()
     {
+       
+
+        //クリックでイガグリを生成し続ける
         if (Input.GetMouseButtonDown(0))
         {
+            GameObject igaguri = Instantiate(igaguriPrafab);
+//            igaguri.GetComponent<igaguriController>().Shoot(new Vector3(0, 200, 2000));
 
-            GameObject igaguri = Instantiate(igaguriPrefab);
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Vector3 wolrdDir = ray.direction;
-            igaguri.GetComponent<IgaguriContoller>().Shoot((wolrdDir.normalized*2000));
-        
+            Vector3 worldDir = ray.direction;
+
+            igaguri.GetComponent<igaguriController>().Shoot(worldDir.normalized*2000);
+
         }
     }
 }
